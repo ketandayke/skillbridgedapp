1b5e3223ae8eaef2159defc7436a7d5412a94f71
ketandayke <ketandayke@gmail.com>
2025-06-29 23:29:08 +0530

    backend corrected

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ extended: true, limit: '200mb' }));
+
 
 app.get("/",(req,res)=>{
       res.send("this is ketan")
